405ba66b98885147b2ad9405700678118138395f
Akzhol <suborovakzhol7@gmail.com>
2022-06-14 16:25:50 +0600

    fixed

---
+++ Week3/POJOclass/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        MobilePhone mobile = new MobilePhone();
+        System.out.println(mobile.getProvidedYear("2022"));
+        System.out.println(mobile.getModel("iphone"));
+
+    }
+}
---
+++ Week3/POJOclass/MobilePhone.java
@@ -0,0 +1,35 @@
+public class MobilePhone {
+    int providedyear;
+    String model;
+    String cityProvided;
+
+    public int getProvidedyear(int i) {
+        return providedyear;
+    }
+
+    public void setProvidedyear(int providedyear) {
+        this.providedyear = providedyear;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getCityProvided() {
+        return cityProvided;
+    }
+
+    public void setCityProvided(String cityProvided) {
+        this.cityProvided = cityProvided;
+    }
+    public String getModel(String model){
+        return model;
+    }
+    public String getProvidedYear(String year){
+        return year;
+    }
+}
